39ae26c0eddc412da1643484d803c223de8f2c5f
cjiao100 <38782743+cjiao100@users.noreply.github.com>
2021-10-10 21:43:11 +0800

    slice

---
+++ sliceops.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func printSlice(s []int) {
+	fmt.Printf("%v len=%d, cap=%d\n", s, len(s), cap(s))
+}
+
+func main() {
+	fmt.Println("Creating Slice")
+
+	var s []int // 当前值是nil
+
+	for i := 0; i < 100; i++ {
+		printSlice(s)
+		s = append(s, 2*i+1)
+	}
+
+	fmt.Println(s)
+
+	s1 := []int{2, 4, 6, 8}
+	printSlice(s1)
+
+	s2 := make([]int, 10)
+	printSlice(s2)
+
+	s3 := make([]int, 10, 32)
+	printSlice(s3)
+
+	fmt.Println("Copying Slice")
+
+	copy(s2, s1)
+	printSlice(s2)
+
+	fmt.Println("Deleting elements from slice")
+
+	// go语言版解构赋值？
+	s2 = append(s2[:3], s2[4:]...)
+	printSlice(s2)
+
+	fmt.Println("Popping from front")
+	front := s2[0]
+	s2 = s2[1:]
+	fmt.Println(front)
+	printSlice(s2)
+
+	fmt.Println("Popping from back")
+	tail := s2[len(s2)-1]
+	s2 = s2[:len(s2)-1]
+
+	fmt.Println(tail)
+	printSlice(s2)
+}
--- slices.go
+++ slices.go
@@ -16,4 +16,21 @@ func main() {
 	arr[4] = 100
 	fmt.Println(arr)
 	fmt.Println(s)
+
+	s1 := arr[2:6]
+	s2 := s1[3:5]
+
+	fmt.Println("s1 = ", s1)
+	fmt.Println("s2 = ", s2)
+
+	// 一个切片中会包含切片开始位置、切片长度、cap创图
+	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d \n", s1, len(s1), cap(s1))
+
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+
+	// s4 和 s5不再是arr的view
+	fmt.Println(s3, s4, s5)
+	fmt.Println(arr)
 }
